02619384926856f7061d486271f1e90f45674a13
Aman Mina <minaaman09@gmail.com>
2022-07-11 23:18:09 +0530

    Fourth Commit

--- My Personal Blog Project/main.py
+++ My Personal Blog Project/main.py
@@ -17,7 +17,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 db = SQLAlchemy(app)
